84b51fc860fac7192dffe58fe5c7e463f0faa4e9
elroypeter <30926117+elroypeter@users.noreply.github.com>
2022-09-22 10:02:21 +0300

    added website breadcrumb

--- src/app/web-pages/Contact-Us/ContactUs.js
+++ src/app/web-pages/Contact-Us/ContactUs.js
@@ -1,10 +1,12 @@
 import React from "react";
+import WebBreadCrumb from "../../components/WebBreadCrumb/WebBreadCrumb";
 import ContactForm from "./ContactForm";
 import ContactMap from "./ContactMap";
 
 export default function ContactUs() {
     return (
         <>
+            <WebBreadCrumb page="Contact Us" />
             <ContactMap />
             <ContactForm />
         </>
